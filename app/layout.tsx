133f5f6c542893f94321c3787263c6856d9f189b
HugoSoy <hugoivanrf@gmail.com>
2026-04-08 21:03:38 -0600

    fix: viewport mobile iOS

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaOrg) }}
