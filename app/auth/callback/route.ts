8654b48fe3f1c3a01342e928c87857e6322f1c4b
HugoSoy <hugoivanrf@gmail.com>
2026-04-17 18:48:51 -0600

    fix: middleware para sincronizar sesion auth

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,15 +6,21 @@ export async function GET(request: NextRequest) {
   const code = searchParams.get('code')
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type')
+  const next = searchParams.get('next') ?? '/'
 
   if (code) {
     const supabase = await createServerSupabaseClient()
-    await supabase.auth.exchangeCodeForSession(code)
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    if (!error) return NextResponse.redirect(`${origin}${next}`)
   }
 
   if (token_hash && type) {
     const supabase = await createServerSupabaseClient()
-    await supabase.auth.verifyOtp({ token_hash, type: type as any })
+    const { error } = await supabase.auth.verifyOtp({ 
+      token_hash, 
+      type: type as any 
+    })
+    if (!error) return NextResponse.redirect(`${origin}${next}`)
   }
 
   return NextResponse.redirect(`${origin}/`)
